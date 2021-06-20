66e364589ef9889ccfdb7d7d117e3fe9c92ea878
Shuri-Honda-1101 <knh065146@gmail.com>
2021-06-20 20:02:19 +0900

    [add]import of firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAR9YaFrNqYJfWWC1lpFBXREZHwmplFq0I",
+  authDomain: "teamb-d552c.firebaseapp.com",
+  projectId: "teamb-d552c",
+  storageBucket: "teamb-d552c.appspot.com",
+  messagingSenderId: "774448339165",
+  appId: "1:774448339165:web:2109b3ff244812d66b6334",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
