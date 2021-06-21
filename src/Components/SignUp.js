af42dc17456cc8601c8550028ef71bac57b54c15
Shuri-Honda-1101 <knh065146@gmail.com>
2021-06-21 17:33:00 +0900

    [add]SignUp.jsの骨組みを作成

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -4,7 +4,32 @@ import firebase from "../config/firebase";
 export const SignUp = () => {
   return (
     <div>
-      <h1>signup</h1>
+      <h1>新規ユーザー登録</h1>
+      <form>
+        <div>
+          <label htmlFor="name">ユーザー名: </label>
+          <input name="name" type="name" id="name" placeholder="ユーザー名" />
+        </div>
+        <div>
+          <label htmlFor="email">メールアドレス: </label>
+          <input
+            name="email"
+            type="email"
+            id="email"
+            placeholder="メールアドレス"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">パスワード: </label>
+          <input
+            name="password"
+            type="password"
+            id="password"
+            placeholder="パスワード"
+          />
+        </div>
+        <button type="submit">新規登録</button>
+      </form>
     </div>
   );
 };
