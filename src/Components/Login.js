ff458e5919424f8dbf10a3705654cedbe33de734
hiroki <hiroki.niijima@icloud.com>
2021-06-24 23:30:56 +0900

    add feature-login

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import firebase from '../config/firebase'
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const hundleSubmit = (e) => {
+        e.preventDefaule()
+        firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .catch(err => {
+            console.log(err)
+        }) 
+
+    }
     return (
         <>
             <h1>Login</h1>        
-            <form>
+            <form onSubmit={hundleSubmit}>
                 <div>
                     <label htmlFor='email'>E-mail</label>
                     <input 
@@ -12,15 +26,25 @@ const Login = () => {
                         id='email' 
                         name='email' 
                         placeholder='Email'
+                        value={email}
+                        onChange ={(e)=>{
+                         setEmail(e.target.value)
+                        }}
+                        
                     />
                 </div>
                 <div>
                     <label htmlFor='password'>Password</label>
-                    <input 
+                    <input
                         type='password' 
                         id='password' 
                         name='password'
                         placeholder='password' 
+                        value={password}
+                        onChange = {(e)=> {
+                            setPassword(e.target.value)
+                        }}
+                        
                     />
                 </div>
                 <button type='submit'>Login</button>
