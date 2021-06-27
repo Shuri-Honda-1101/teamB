d7882ee664b00503f9f6dcc10113ad3824cdb6c1
Shuri-Honda-1101 <knh065146@gmail.com>
2021-06-27 23:02:05 +0900

    [add]お酒一覧を表示する機能

--- src/Components/Catalog.js
+++ src/Components/Catalog.js
@@ -1,7 +1,50 @@
+import { useEffect, useState } from "react";
+import firebase from "../config/firebase";
+// import { AuthContext } from "../AuthServise";
+import { Link } from "react-router-dom";
+import DrinkItem from "./DrinkItem";
+
 const Catalog = () => {
+  const [drinks, setDrinks] = useState(null);
+
+  // const user = useContext(AuthContext);
+  const user = firebase.auth().currentUser;
+
+  //お酒リストを取ってくる処理
+  useEffect(() => {
+    if (user != null) {
+      console.log(user.uid);
+      firebase
+        .firestore()
+        .collection(user.uid)
+        .onSnapshot((querySnapshot) => {
+          const drinks = querySnapshot.docs.map((doc) => {
+            return { ...doc.data(), id: doc.id };
+          });
+          setDrinks(drinks);
+        });
+    }
+  }, [user]);
+  console.log(drinks);
+
   return (
     <div>
       <h1>ここはCatalogコンポーネントです</h1>
+      <Link to="/edit">新規メモ作成</Link>
+      <ul>
+        {drinks &&
+          drinks.map((drink) => {
+            return (
+              <DrinkItem
+                key={drink.id}
+                drink={drink.drink}
+                rate={drink.rate}
+                image={drink.image}
+                tags={drink.tags}
+              />
+            );
+          })}
+      </ul>
     </div>
   );
 };
---
+++ src/Components/DrinkItem.js
@@ -0,0 +1,17 @@
+const DrinkItem = ({ key, drink, rate, tags, image }) => {
+  return (
+    <li>
+      <img src={image} height={100} />
+      <h2>{drink}</h2>
+      <p>レーティング: {rate}</p>
+      <h3>タグ</h3>
+      <ul>
+        {tags.map((tag) => {
+          return <li>{tag}</li>;
+        })}
+      </ul>
+    </li>
+  );
+};
+
+export default DrinkItem;
