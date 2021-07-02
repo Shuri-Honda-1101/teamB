02f2e412e10a2eb1054bb8e8d86d1cbb66027734
Shuri-Honda-1101 <knh065146@gmail.com>
2021-07-02 15:19:39 +0900

    [remove]antdesignのアンインストール

--- src/Components/DrinkItem.js
+++ src/Components/DrinkItem.js
@@ -1,19 +1,19 @@
-// import shortid from "shortid";
+import shortid from "shortid";
 
-// const DrinkItem = ({ drink, rate, tags, image }) => {
-//   return (
-//     <li>
-//       <img src={image} height={100} alt={drink} />
-//       <h2>{drink}</h2>
-//       <p>レーティング: {rate}</p>
-//       <h3>タグ</h3>
-//       <ul>
-//         {tags.map((tag) => {
-//           return <li key={shortid.generate()}>{tag}</li>;
-//         })}
-//       </ul>
-//     </li>
-//   );
-// };
+const DrinkItem = ({ drink, rate, tags, image }) => {
+  return (
+    <li>
+      <img src={image} height={100} alt={drink} />
+      <h2>{drink}</h2>
+      <p>レーティング: {rate}</p>
+      <h3>タグ</h3>
+      <ul>
+        {tags.map((tag) => {
+          return <li key={shortid.generate()}>{tag}</li>;
+        })}
+      </ul>
+    </li>
+  );
+};
 
-// export default DrinkItem;
+export default DrinkItem;
