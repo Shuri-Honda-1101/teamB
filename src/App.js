a26ded6a2d44f09a2d49ff0237d3e777e90b6d63
塩川歩 <shiokawaayumi@shiokawaayuminoMacBook-Pro.local>
2021-07-02 22:53:13 +0900

    ログイン状態によるページの切り替え機能

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Edit from "./Components/Edit";
 import Item from "./Components/Item";
 import Catalog from "./Components/Catalog";
 import { SignUp } from "./Components/SignUp";
 import Login from "./Components/Login";
+//import firebase from "./config/firebase";
+import { AuthProvider } from "./Components/AuthService"
+import LoggedInRoute from "./Components/LoggedInRoute";
+
 
 const App = () => {
   return (
     <div>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Catalog} />
-          <Route exact path="/edit" component={Edit} />
-          <Route exact path="/item" component={Item} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <LoggedInRoute exact path="/" component={Catalog} />
+            <Route exact path="/edit" component={Edit} />
+            <Route exact path="/item" component={Item} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </Router>
+      </AuthProvider>
     </div>
   );
 };
--- src/Components/Catalog.js
+++ src/Components/Catalog.js
@@ -1,3 +1,4 @@
+
 const Catalog = () => {
   return (
     <div>
